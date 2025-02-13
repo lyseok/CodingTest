7ae57731b21ac647eea67b664ae7e917e6ff42a1
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-02-14 00:51:12 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		int[] result = new int[T]; 
+		
+		for(int i = 0 ; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			result[i] = A + B;
+		}
+		for(int i : result) {
+			System.out.println(i);
+		}
+	}
+
+}
