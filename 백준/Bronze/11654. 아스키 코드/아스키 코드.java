d1ac879a5eae51c030d1466d4e0ebe152554cd8f
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-02-14 01:00:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        char ch = st.nextToken().charAt(0);
+        
+        bw.write(String.valueOf((int)ch));
+        bw.flush();
+        bw.close();
+
+	}
+
+}
