7a289c046245f37e81730c2cf40a3ad366f8622c
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-02-14 01:08:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+
+    	int N = Integer.parseInt(st.nextToken());
+    	int K = Integer.parseInt(st.nextToken());
+    	int count = 0;
+    	int num = 0;
+    	for(int i = 1; i <= N; i++) {
+    		if(N % i == 0) {
+    			count++;
+    		}
+    		if(K == count) {
+    			num = i;
+    			break;
+    		}
+    		else {
+    			num = 0;
+    		}
+    	}
+    	bw.write(String.valueOf(num));
+    	br.close();
+    	bw.flush();
+    	bw.close();
+	}
+}
