9ca55b146e34f35a5dc30c2a8bb024092c06c17e
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-02-14 01:11:22 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 120 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+	private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+    public static void main(String[] args) throws IOException {
+    	new Main().algorithm();
+	}
+    
+    public void algorithm() throws IOException {
+    	long input = Long.parseLong(br.readLine());
+    	bw.write(input * input + "\n");
+    	bw.write("2\n");
+    	
+    	bw.flush();
+    	bw.close();
+    }
+
+}
